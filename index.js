77e9940659219fee030ce50cff520af56084c44b
zachariah martin <zachariahsmartin@gmail.com>
2022-10-02 18:18:40 -0400

    commit tree 1f1700d5baa42e8e1a7774594e7746d96f0e0b06

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
+
+function isPalindrome(word) {
+  const len= word.length
+  for (let i=0; i < len/2; i++)
+  if (word[i] !== word[len - 1 - i]) {
+    return false
+  }
+  return true
+  }
+/*
+Write a function isPalindrome that will receive one argument, a string. 
+Should return true if the string is a palindrome.
+Should return false if it is not a palindrome.
+*/
 /* 
-  Add your pseudocode here
+  find length of string
+    declare const = string length
+  loop through half of the string
+    for loop, starting at 0, with i les than length/2, i incremented by 1
+      if string(i) is strictly equal to string(length - 1 - i)
+        return true
+  return false
+    
+  
+
 */
 
 /*
-  Add written explanation of your solution here
+  Explenation, the function isPalindrome takes in a string as an argument. It 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("rotator"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("moron"));
+  
+  console.log("");
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
